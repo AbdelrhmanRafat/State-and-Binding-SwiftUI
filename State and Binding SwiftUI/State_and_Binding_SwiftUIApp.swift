c05aee64927745837d5a2361c89a198d9fcffc98
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 14:46:17 +0200

    Initial Commit

---
+++ State and Binding SwiftUI/State_and_Binding_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_and_Binding_SwiftUIApp.swift
+//  State and Binding SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct State_and_Binding_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

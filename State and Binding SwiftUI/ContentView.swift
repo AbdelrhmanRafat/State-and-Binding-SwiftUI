4ae4026e495c8c9c04e9d1c6783e65fc07649ce4
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-10 15:50:43 +0200

    ‘Controlling_Button_State’

--- State and Binding SwiftUI/ContentView.swift
+++ State and Binding SwiftUI/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var isPlaying = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            self.isPlaying.toggle()
+        })
+        {
+            Image(systemName: isPlaying ?  "stop.circle.fill" : "play.circle.fill")
+                .font(.system(size: 150))
+                .foregroundColor(isPlaying ? .red : .green)
+        }
     }
 }
 

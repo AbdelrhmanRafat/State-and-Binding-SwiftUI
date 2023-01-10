8c8f4ff8ad3f159661bf72b30a29d2e63862e3ad
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-10 20:00:09 +0200

    Working With Binding.

--- State and Binding SwiftUI/ContentView.swift
+++ State and Binding SwiftUI/ContentView.swift
@@ -9,31 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
    @State private var Counter = 1 //@state let property Accessed from the View's body (or from function Called by it) // That's why you should State Proprties as Private.
-    @State private var paddingSize : CGFloat = 22 // Intial Size for Circle.
-    @State private var IncrementPadding = 10 // Increase Circle Size Every time Numbers Increases digit.
     var body: some View {
-        Button(action: {
-            Counter += 1 // Increment Counter by One For every Tap
-            if(Counter == IncrementPadding)
-            {
-               paddingSize += 10
-               IncrementPadding *= 10
-            }
-        })
-        {
-                 Text(String(Counter))
-                .font(.system(size: 100))
-                .fontWeight(.semibold)
-                .foregroundColor(.white)
-                .padding(paddingSize)// Padding Get Before Background Color control size of circle.
-                .background(Color.red)
-                .clipShape(Circle())
-        }
+        CounterButton(Counter: $Counter)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CounterButton : View {
+    @Binding var Counter : Int
+    var body: some View {
+        Button(action: {
+            Counter += 1 // Increment Counter by One For every Tap
+        })
+        {
+            Circle()
+                .frame(width: 200, height: 200, alignment: .center)
+                .foregroundColor(.red)
+                .overlay(
+                Text("\(Counter)")
+                    .font(.system(size: 100, weight: .bold, design: .rounded))
+                    .foregroundColor(.white)
+                )
+        }
     }
 }
